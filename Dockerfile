ec8d820526bf93932488f4e771713a75f073b4b3
Mohammad <mrkaskani@gmail.com>
2023-01-05 20:58:20 +0330

    add uvicorn to main.py

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,4 @@ RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 
 COPY . /code/app
 
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD python ./code/app/main.py
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from coordinate.noominatim import RandomCoordinate
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+import uvicorn
+
 app = FastAPI()
 
 
@@ -15,3 +17,7 @@ class Coordinate(BaseModel):
 def coordinate(item: Coordinate):
     random = RandomCoordinate(location=item.location, quantity=item.quantity)
     return {"data": random.generate_random_coordinate()}
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
